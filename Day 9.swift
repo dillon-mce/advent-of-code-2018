82820ac50ab8ddac183361946e7da1b8f6f2fb24
Dillon McElhinney <dillon.mce@gmail.com>
2018-12-11 13:40:30 -0700

    Finish part 2, day 9

--- Day 9.swift
+++ Day 9.swift
@@ -40,8 +40,8 @@ func calculateHighScore(_ string: String, multiplier: Int) -> Int {
     var scores: [Int: Int] = [:]
     let numOfPlayers = input[0]
     let rounds = input[1] * multiplier
-//    let possibleWinner = rounds % numOfPlayers != 0 ? rounds % numOfPlayers : numOfPlayers
-//    print("Possible winner: \(possibleWinner)")
+    //    let possibleWinner = rounds % numOfPlayers != 0 ? rounds % numOfPlayers : numOfPlayers
+    //    print("Possible winner: \(possibleWinner)")
     
     for i in 1...rounds {
         let startTime = CFAbsoluteTimeGetCurrent()
@@ -58,29 +58,30 @@ func calculateHighScore(_ string: String, multiplier: Int) -> Int {
             let newMarble = Marble(score: i)
             newMarble.insert(between: currentMarble.offset(1), and: currentMarble.offset(2))
             currentMarble = newMarble
-            print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to add marble \(i)")
+            //print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to add marble \(i)")
         }
     }
     
-//    for (player, score) in scores.sorted(by: { $0.key < $1.key }) {
-//        //print("Player \(player) got a score of: \(score)")
-//    }
+    //    for (player, score) in scores.sorted(by: { $0.key < $1.key }) {
+    //        //print("Player \(player) got a score of: \(score)")
+    //    }
     
     guard let winner = scores.max(by: { $0.value < $1.value }) else { return -1 }
     print("Player \(winner.key) won with a score of \(winner.value)")
-    print("That score is a factor of \(input[1]) with a remainder of \(winner.value % input[1])")
+    //print("That score is a factor of \(input[1]) with a remainder of \(winner.value % input[1])")
     return winner.value
 }
 
 let testInput1 = "9 players; last marble is worth 23 points" // 32
 //for i in 1...20 {
-    calculateHighScore(testInput1, multiplier: 100)
+//calculateHighScore(testInput1, multiplier: 100)
 //}
-let testInput2 = "9 players; last marble is worth 2500 points" //8317
-//calculateHighScore(testInput2)
+let testInput2 = "10 players; last marble is worth 1618 points" //8317
+//calculateHighScore(testInput2, multiplier: 1)
 let testInput3 = "13 players; last marble is worth 7999 points" //146373
 //calculateHighScore(testInput3)
+let day9Input = "439 players; last marble is worth 71307 points"
+calculateHighScore(day9Input, multiplier: 100)
 
-//calculateHighScore(day9Input, multiplier: 100)
+//Answer:
 
-//Answer: 3314195047
